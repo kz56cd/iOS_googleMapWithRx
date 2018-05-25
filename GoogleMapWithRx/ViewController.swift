e450166f4d4b274f94c558bb8f0328a78ce188c5
kz56cd <kz56cd@gmail.com>
2018-05-25 13:23:54 +0900

    Init project

---
+++ GoogleMapWithRx/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GoogleMapWithRx
+//
+//  Created by Masakazu Sano on 2018/05/25.
+//  Copyright © 2018年 Masakazu Sano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
