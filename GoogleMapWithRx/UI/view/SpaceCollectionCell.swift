48fc6eebd8d8d8730429142f877bfb629857a490
kz56cd <kz56cd@gmail.com>
2018-06-08 12:38:28 +0900

    Add space cell

---
+++ GoogleMapWithRx/UI/view/SpaceCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  SpaceCollectionCell.swift
+//  GoogleMapWithRx
+//
+//  Created by Masakazu Sano on 2018/06/08.
+//  Copyright © 2018年 Masakazu Sano. All rights reserved.
+//
+
+import UIKit
+
+class SpaceCollectionCell: UICollectionViewCell {
+    
+}
