1608cfffccfa5d3b97de0d9285804971f6f2a851
kz56cd <kz56cd@gmail.com>
2018-06-08 11:27:18 +0900

    Add space model

---
+++ GoogleMapWithRx/Model/Space.swift
@@ -0,0 +1,17 @@
+//
+//  Space.swift
+//  GoogleMapWithRx
+//
+//  Created by Masakazu Sano on 2018/06/08.
+//  Copyright © 2018年 Masakazu Sano. All rights reserved.
+//
+
+import Foundation
+
+struct Space {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let minPrice: Int
+}
